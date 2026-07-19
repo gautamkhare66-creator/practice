528d671e70177b865225b192f1467a7fc4a6b38f
Jack <gautamkhare66@gmail.com>
2026-07-19 12:06:22 +0530

    Toggle mode button

--- Practice/script.js
+++ Practice/script.js
@@ -1,61 +1,13 @@
-console.log("Script.js initializing")
-// let boxes = document.getElementsByClassName("box")
-let boxes = document.querySelector("#container").children 
+let btn = document.getElementById("btn");
+let body = document.querySelector("body");
 
-
-function getRandomColor(){
-    let val1 = Math.ceil(0 + Math.random()* 255);
-    let val2 = Math.ceil(0 + Math.random()* 255);
-    let val3 = Math.ceil(0 + Math.random()* 255);
-    return `rgb(${val1}, ${val2}, ${val3})`
+const toggle = () => {
+    if (body.style.backgroundColor === "black") {
+        body.style.backgroundColor = "white";
+    } else {
+        body.style.backgroundColor = "black";
+    }
 }
 
-Array.from(boxes).forEach(e=>{
-    e.style.backgroundColor = getRandomColor()
-    e.style.color = getRandomColor()
-})
-
-const handler=()=>{
-    console.log("You clicked box 1.");
-}
-
-box1.addEventListener("click",handler);
-
-box1.removeEventListener("click",handler);
-
-box2.onclick=()=>{
-    console.log("box2 clicked");
-}
-
-box3.onclick=()=>{
-    console.log("box3 clicked");
-}
-
-box4.onclick=()=>{
-    console.log("box4 clicked");
-}
-
-box5.onclick=()=>{
-    console.log("box5 clicked");
-}
-
-box1.onmouseover=()=>{
-    console.log("You are in box1");
-}
-
-box2.onmouseover=()=>{
-    console.log("You are in box2");
-}
-
-box3.onmouseover=()=>{
-    console.log("You are in box3");
-}
-
-box4.onmouseover=()=>{
-    console.log("You are in box4");
-}
-
-box5.onmouseover=()=>{
-    console.log("You are in box5");
-}
+btn.addEventListener("click", toggle);
 
